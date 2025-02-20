6a8d9fa7cca6c497fceca7e8f1b8b030784e263d
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-02-20 11:28:18 +0300

    num1

---
+++ Avto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac24
+{
+    internal class Avto
+    {
+        /// <summary>
+        /// скрытые поля
+        /// </summary>
+        string brand;
+        string color;
+        double skor;
+        /// <summary>
+        /// Инкапсуляция полей
+        /// </summary>
+        public string Brand { get { return brand;} set { brand = value; } }
+        public string Color { get { return color; } set { color = value; } }
+        public double Skor { get { return skor; } set { if (value < 20) { skor = 20; } else if (value > 120) { skor = 120; } else { skor = value; } } }
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Avto() { }
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        /// <param name="brand"></param>
+        /// <param name="color"></param>
+        /// <param name="skor"></param>
+        public Avto(string brand, string color, double skor) 
+        { 
+        this.Brand = brand;
+        this.Color = color;
+        this.Skor = skor;
+        }
+        /// <summary>
+        /// метод для вывода значений об объекте.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Brand = {Brand}, Color = {Color}, Skor = {Skor}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,22 @@ namespace prac24
     {
         static void Main(string[] args)
         {
+            //объект с именем avto,конструктор без параметров
+            Avto avto = new Avto();
+            Console.WriteLine(avto.ToString());
+            //объект с именем avto,конструктор c параметрами
+            Avto avto2 = new Avto("БМВ", "Желтый", 220);
+            Console.WriteLine(avto2.ToString());
+            //конструктор с parse
+            Console.WriteLine("Введите марку");
+            string brand = Console.ReadLine();
+            Console.WriteLine("Введите цвет");
+            string color = Console.ReadLine();
+            Console.WriteLine("Введите скорость");
+            double skor = double.Parse(Console.ReadLine());
+            Avto avto3 = new Avto(brand,color,skor);
+            Console.WriteLine($"{avto3.ToString()}\n");
+            Console.ReadLine();
         }
     }
 }
